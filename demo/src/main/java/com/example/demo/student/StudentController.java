a3c3e0ff656d72ca00e4718fafa27ddf78d89277
codyb <cody084@revature.com>
2022-05-18 13:53:12 -0400

    First commit to demo2 Springboot

---
+++ demo/src/main/java/com/example/demo/student/StudentController.java
@@ -0,0 +1,31 @@
+package com.example.demo.student;
+
+import com.sun.xml.bind.v2.schemagen.xmlschema.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "api/v1/student")
+public class StudentController {
+    private final StudentService studentService;
+
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping
+    public List<Student> getStudents(){
+        return studentService.getStudents();
+    }
+
+    @PostMapping
+    public void registerNewStudent(@RequestBody Student student){
+        studentService.addNewStudent(student);
+    }
+
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudent(@PathVariable("studentId") Long studentId) {
+        studentService.deleteStudent(studentId);
+    }
+}
